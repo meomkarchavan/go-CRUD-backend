873acd5781e174ba2c22a077685774bc7ff6acc9
meomkarchavan <meomkarchavan99@gmail.com>
2021-08-30 13:24:00 +0530

    set ACCESS_SECRET

--- src/main.go
+++ src/main.go
@@ -1,9 +1,12 @@
 package main
 
-import "blog_rest_api_gin/src/controller"
+import (
+	"blog_rest_api_gin/src/controller"
+	"os"
+)
 
 func main() {
-
+	os.Setenv("ACCESS_SECRET", "omkar")
 	r := controller.RegisterRoutes()
 	r.Run("127.0.0.1:8080")
 
